c94145ba9c6f9ddc06ef5c5c56be47c0a2413191
Berlin <tommyonik@gmail.com>
2025-01-21 22:06:14 -0500

    add conditional header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "./components/Footer";
-
+import Header from "./components/Header";
 export const metadata: Metadata = {
   title: "TNMA",
   description: "Tnma's personal website",
@@ -14,7 +14,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="min-h-screen flex flex-col mx-auto px-16 py-16">
+      <body className="min-h-screen flex flex-col max-w-6xl mx-auto px-16 py-16">
+        <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
       </body>

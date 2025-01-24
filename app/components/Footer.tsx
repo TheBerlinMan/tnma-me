016604b1990b520a3bfe101707931fe60629efd3
Berlin <tommyonik@gmail.com>
2025-01-24 12:25:51 -0500

    add fun message to fotter

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,12 +4,25 @@ import { getCurrentDateLong } from '@/lib/functions'
 import React from 'react'
 
 const Footer = () => {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth(); // 0-11 where 0 is January
+  
+  const getMessage = () => {
+    if (currentMonth === 0 || currentMonth === 1) { // January or February
+      return "PS: Cara cara oranges are in season";
+    } else if (currentMonth >= 11 || currentMonth <= 3) { // December to April
+      return "PS: Cara cara oranges are in season";
+    } else {
+      return "&quot;Just keep swimming.&quot;";
+    }
+  };
+  
   return (
     <footer className="mt-auto text-gray-500 text-sm text-center py-4">
       <hr className="border-gray-500 border-1 mb-4" />
       <div className="flex justify-between">
         <p className="text-gray-500 text-sm">
-          &quot;Just keep swimming.&quot;
+          {getMessage()}
         </p>
         {/* <p>© {getCurrentDateLong()}</p> */}
         <p>{getCurrentDateLong()}</p>

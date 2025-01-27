0944d16b6b848cdbee4dd0c899a853207190d52d
Berlin <tommyonik@gmail.com>
2025-01-26 20:36:25 -0500

    make about copy responsive

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,54 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+
+const useIsMobile = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 640);
+    };
+    // Set initial value
+    handleResize();
+    // Add event listener
+    window.addEventListener('resize', handleResize);
+    // Cleanup
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return isMobile;
+};
 
 const About = () => {
+  const isMobile = useIsMobile();
+  return (
+    <>
+      {isMobile ? <AboutMobile /> : <AboutDesktop />}
+    </>
+  );
+};
+
+export default About;
+
+const AboutMobile = () => {
+  return (
+    <div className="flex flex-col gap-6 text-sm">
+      <div className="flex flex-col gap-2">
+        <p>My name is Tommy David Onik and I&apos;m based in Brooklyn, NY. &apos;TNMA&apos; is my russian name written in
+        english letters. You can just call me Tommy, though.</p>
+      </div>
+      <div className="flex flex-col gap-2">
+        <p>
+          My undergraduate thesis advisor once called me a polymath and I liked that
+          - maybe too much. I code, I play chess, I design jewelry, I take photos, I draw, I cook,
+          I dance, I fight. Life is more fun as a generalist, I think.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const AboutDesktop = () => {
   return (
     <div className="flex flex-col gap-6 text-sm">
       <div className="flex flex-col gap-2">
@@ -9,7 +57,6 @@ const About = () => {
           &apos;TNMA&apos; is my russian name written in
           english letters.
         </p>
-        {/* <p>It&apos;s pronounced &apos;Tee-muh&apos;.</p> */}
         <p>
           You can just call me Tommy, though.
         </p>
@@ -27,12 +74,6 @@ const About = () => {
           Life is more fun as a generalist, I think.
         </p>
       </div>
-      {/* <div className="flex flex-col gap-3">
-        <p>If you&apos;re here, thank you.</p>
-        
-      </div> */}
     </div>
   );
 };
-
-export default About;

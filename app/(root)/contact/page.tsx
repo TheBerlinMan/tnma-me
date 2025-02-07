507eb27ae7a6b2174f996e90bc11a7c34f1535c2
Berlin <tommyonik@gmail.com>
2025-02-06 22:48:29 -0500

    edit emailform

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -4,7 +4,8 @@ import EmailForm from "../components/EmailForm";
 const Contact = () => {
   return (
       <div className="w-full max-w-xl fade-in">
-        <p className="text-sm mb-4">
+        <h1 className="text-lg font-bold mb-1">Send A Message</h1>
+        <p className="text-sm mb-6">
           I&apos;d love to meet more people. If you&apos;d like your photo
           taken, a website built, or just want to grab a coffee, please get in touch.
         </p>

692efcf13d564562412e3f69e74ec563bdc5b560
Berlin <tommyonik@gmail.com>
2025-02-06 22:51:57 -0500

    resolve build failure

--- app/(root)/components/Footer.tsx
+++ app/(root)/components/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { getCurrentDateLong } from "@/lib/functions";
+// import { getCurrentDateLong } from "@/lib/functions";
 import { Github, Instagram, Linkedin } from "lucide-react";
 import Link from "next/link";
 import React from "react";

69232694d5b35c626f7e25b90963b69fe757031f
Berlin <tommyonik@gmail.com>
2025-02-09 23:03:17 -0500

    delete files and build jewelry page

--- app/(root)/art/jewelry/page.tsx
+++ app/(root)/art/jewelry/page.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
 
-const page = () => {
+import React from "react";
+import fs from "fs";
+import path from "path";
+
+import CarouselPage from "@/app/(root)/components/CarouselPage";
+
+async function getJewelry() {
+  const jewelryDir = path.join(process.cwd(), "public", "jewelry");
+  return fs.readdirSync(jewelryDir).map((file) => `/jewelry/${file}`);
+}
+
+const page = async () => {
+  const jewelryFiles = await getJewelry();
   return (
-    <div>page</div>
+    <div className="fade-in max-w-xl">
+      <CarouselPage images={jewelryFiles} />
+    </div>
   )
 }
 

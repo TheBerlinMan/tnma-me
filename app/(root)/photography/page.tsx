b0be078041fecb63473f1587b43ff35c7baebfc7
Berlin <tommyonik@gmail.com>
2025-07-06 18:55:44 -0300

    finish routing and templates for all photography pages

--- app/(root)/photography/page.tsx
+++ app/(root)/photography/page.tsx
@@ -4,22 +4,22 @@ import Link from "next/link";
 const Photography = async () => {
   return (
     <div className="fade-in ml-4">
-        <div>Index</div>
-        <div className="">
-          <div>Portrait</div>
-          <Link href="/photography/portraits">Portraits</Link>
-          <div>Landscape</div>
-          <div>Street</div>
-          <div>Abstract</div>
-        </div>
-      
+      <div className="flex flex-col gap-1">
+        <Link href="/photography/index">Index</Link>
+        <Link href="/photography/portraits">Portraits</Link>
+        <Link href="/photography/landscape">Landscape</Link>
+        <Link href="/photography/street">Street</Link>
+        <Link href="/photography/abstract">Abstract</Link>
+      </div>
 
       <div className="mt-6">Series In Progress</div>
-      <div className="ml-4 mt-2 italic text-sm">
-        <div>... Public Intimacy</div>
-        <div>... The Nape</div>
-        <div>... Portals</div>
-        <div>... NYC</div>
+      <div className="ml-4 mt-2 italic text-sm flex flex-col gap-1">
+        <Link href="/photography/series/publicintimacy">
+          ... Public Intimacy
+        </Link>
+        <Link href="/photography/series/nape">... The Nape</Link>
+        <Link href="/photography/series/portals">... Portals</Link>
+        <Link href="/photography/series/nyc">... NYC</Link>
       </div>
     </div>
   );

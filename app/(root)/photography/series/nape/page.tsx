d6ff86491789691b7ea49b3f9687e5231f7a0247
Berlin <tommyonik@gmail.com>
2025-07-07 12:29:36 -0300

    write draft of series description

--- app/(root)/photography/series/nape/page.tsx
+++ app/(root)/photography/series/nape/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import BackRedirect from "@/app/(root)/components/BackRedirect";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div>
+      <BackRedirect />
+      <div className="max-w-xl font-light">
+        The Nape is an ongoing series of photographs dedicated to documenting
+        the subtleties of the true self in public spaces. When a person knows
+        that their portrait is being taken, a wall is subconsciously erected
+        which hides the persons honest self. A persons true self is on display
+        only within the moments when they forget that they are being observed.
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;

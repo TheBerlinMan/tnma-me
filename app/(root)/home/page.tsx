617a17ffc68717d32be88f2fe1c651684f97ae07
Berlin <tommyonik@gmail.com>
2025-07-11 10:52:00 -0400

    update home layout

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -1,29 +1,23 @@
-
 import Link from "next/link";
 import { ArrowUpRight } from "lucide-react";
 
 const Home = () => {
-
-
   return (
     <div className="flex flex-col gap-6 fade-in">
       <div className="text-sm text-gray-500 -mt-8">
-        {/* <p>based in NYC</p> */}
         <p>b. 03131996</p>
       </div>
       <div className="font-light mt-2 font-light">
         <p>First-generation American-Soviet-Jew</p>
-        {/* <p>Artist & Designer</p> */}
-        {/* <p>Detail-obsessed generalist</p> */}
         <p>Detail-Obsessed Generalist</p>
-        {/* <p>Generalist in love with theory crafting</p> */}
+        <p>Artist, Designer, & Programmer</p>
         <p>Based in NYC</p>
       </div>
 
       <div className=" mt-4 font-light">
         <div className="flex gap-16">
           <div className="flex flex-col">
-            <div className="text-sm font-light mb-4 text-gray-500">Work</div>
+            <div className="text-sm font-light mb-2 text-gray-500">Work</div>
             <div>Software Engineer @ Evernorth</div>
             <div className="flex items-center gap-1">
               Founder @
@@ -35,32 +29,23 @@ const Home = () => {
               </Link>
               <ArrowUpRight strokeWidth="1px" size={"16px"} />
             </div>
-            <div className="text-sm font-light my-4 text-gray-500">
+            <div className="text-sm font-light mt-6 mb-2 text-gray-500">
               Past Work
             </div>
             <div>Financial Analyst @ MediaMath</div>
             <div>FP&A + PPNR Modeler @ UBS</div>
           </div>
           <div className="flex flex-col">
-            <div className="text-sm font-light mb-4 text-gray-500">Hobbies</div>
-            <Link
-              href="/photography"
-              className="underline"
-            >
+            <div className="text-sm font-light mb-2 text-gray-500">Hobbies</div>
+            <Link href="/photography" className="underline">
               Photography
             </Link>
 
-            <Link
-              href="/drawings"
-              className="underline"
-            >
+            <Link href="/drawings" className="underline">
               Drawing
             </Link>
 
-            <Link
-              href="/projects"
-              className="underline"
-            >
+            <Link href="/projects" className="underline">
               Making Things
             </Link>
 
@@ -75,7 +60,7 @@ const Home = () => {
             </div>
           </div>
           <div className="flex flex-col">
-            <div className="text-sm font-light mb-4 text-gray-500">
+            <div className="text-sm font-light mb-2 text-gray-500">
               Education
             </div>
             <div>Software Engineering Bootcamp @ General Assembly</div>
@@ -88,7 +73,6 @@ const Home = () => {
           </div>
         </div>
       </div>
-
     </div>
   );
 };

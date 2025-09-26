64a62051b7f25152c5750bc85996cb430ccbe7c1
Berlin <tommyonik@gmail.com>
2025-09-25 21:29:12 -0400

    make homepage mobile friendly

--- app/page.tsx
+++ app/page.tsx
@@ -3,84 +3,89 @@ import { ArrowUpRight } from "lucide-react";
 import Header from "@/app/(root)/components/Header";
 import Footer from "@/app/(root)/components/Footer";
 
-const Home = () => {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <div className="flex-1 mx-7">
-        <div className="flex flex-col gap-6 fade-in">
-          <div className="font-light font-light">
-            <p>First-generation American-Soviet-Jew</p>
-            <p>Detail-Obsessed Artist, Designer, & Programmer</p>
-            <p>Based in NYC</p>
-          </div>
+const Home = () => (
+  <div className="min-h-screen flex flex-col">
+    <Header />
+    <div className="flex-1 mx-7">
+      <div className="flex flex-col gap-6 fade-in">
+        <div className="font-light font-light">
+          <p>First-generation American-Soviet-Jew</p>
+          <p>Detail-Obsessed Artist, Designer, & Programmer</p>
+          <p>Based in NYC</p>
+        </div>
 
-          <div className=" mt-4 font-light">
-            <div className="flex gap-16">
-              <div className="flex flex-col">
-                <div className="text-sm font-light mb-2 text-gray-500">Work</div>
-                <div>Software Engineer @ Evernorth</div>
-                <div className="flex items-center gap-1">
-                  Founder @
-                  <Link
-                    href="https://domaproject.vercel.app/"
-                    className="underline"
-                  >
-                    Doma
-                  </Link>
-                  <ArrowUpRight strokeWidth="1px" size={"16px"} />
-                </div>
-                <div className="text-sm font-light mt-6 mb-2 text-gray-500">
+        <div className="mt-4 font-light">
+          <div className="flex flex-col gap-6 md:flex-row md:gap-16">
+            <div className="flex flex-col">
+              <div className="text-sm font-light mb-2 text-gray-500">Work</div>
+              <div>Software Engineer @ Evernorth</div>
+              <div className="flex items-center gap-1">
+                Founder @
+                <Link
+                  href="https://domaproject.vercel.app/"
+                  className="underline"
+                >
+                  Doma
+                </Link>
+                <ArrowUpRight strokeWidth="1px" size={"16px"} />
+              </div>
+              <div className="hidden md:flex flex-col mt-6">
+                <div className="text-sm font-light mb-2 text-gray-500">
                   Past Work
                 </div>
                 <div>Financial Analyst @ MediaMath</div>
                 <div>PPNR Modeler @ UBS</div>
               </div>
-              <div className="flex flex-col">
-                <div className="text-sm font-light mb-2 text-gray-500">Hobbies</div>
-                <Link href="/photography" className="underline">
-                  Photography
-                </Link>
+            </div>
+            <div className="flex flex-col">
+              <div className="text-sm font-light mb-2 text-gray-500">Hobbies</div>
+              <Link href="/photography" className="underline">
+                Photography
+              </Link>
 
-                <Link href="/drawings" className="underline">
-                  Drawing
-                </Link>
+              <Link href="/drawings" className="underline">
+                Drawing
+              </Link>
 
-                <Link href="/projects" className="underline">
-                  Projects
-                </Link>
+              <Link href="/projects" className="underline">
+                Making Things
+              </Link>
 
-                <div className="flex items-center gap-1">
-                  <Link
-                    href="https://www.chess.com/member/pigeonmania"
-                    className="underline"
-                  >
-                    Chess
-                  </Link>
-                  <ArrowUpRight strokeWidth="1px" size={"16px"} />
-                </div>
+              <div className="flex items-center gap-1">
+                <Link
+                  href="https://www.chess.com/member/pigeonmania"
+                  className="underline"
+                >
+                  Chess
+                </Link>
+                <ArrowUpRight strokeWidth="1px" size={"16px"} />
               </div>
-              <div className="flex flex-col">
-                <div className="text-sm font-light mb-2 text-gray-500">
-                  Education
-                </div>
-                <div>Software Engineering Bootcamp @ General Assembly</div>
-                <div>
-                  M.Arch @ The Pratt Institute{" "}
-                  <span className="text-xs text-gray-500 italic">(inc)</span>
-                </div>
-                <div>Graphic Design + Color Theory @ SVA</div>
-                <div>B.A. in Mathematics @ Pace University</div>
+            </div>
+            <div className="flex flex-col">
+              <div className="text-sm font-light mb-2 text-gray-500">
+                Education
               </div>
+              <div>Software Engineering Bootcamp @ General Assembly</div>
+              <div>
+                M.Arch @ The Pratt Institute{" "}
+                <span className="text-xs text-gray-500 italic">(inc)</span>
+              </div>
+              <div>Graphic Design + Color Theory @ SVA</div>
+              <div>B.A. in Mathematics @ Pace University</div>
+            </div>
+            <div className="flex flex-col md:hidden">
+              <div className="text-sm font-light mb-2 text-gray-500">Past Work</div>
+              <div>Financial Analyst @ MediaMath</div>
+              <div>PPNR Modeler @ UBS</div>
             </div>
           </div>
         </div>
       </div>
-      <div>
-        <Footer />
-      </div>
     </div>
-  );
-};
+    <div>
+      <Footer />
+    </div>
+  </div>
+);
 
 export default Home;
